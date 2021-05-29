017316f8484b1d43b28fe104e0054912b4bae43d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-29 14:02:43 +0300

    Rokas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee737a09671c0299425b"; }
+/******/ 		__webpack_require__.h = function() { return "9a4a6ea3e120613681cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ function Header() {
                             <p>{user ? `Hello, ${user.email}` : "Sign in guest 🚀"}</p>
                             <p className="font-extrabold md:text-sm">Account & Lists</p>
                         </div>
-                        <div className="link">
+                        <div onClick={() => router.push('/orders')} className="link">
                             <p>Returns</p>
                             <p className="font-extrabold md:text-sm">& Orders</p>
                         </div>
